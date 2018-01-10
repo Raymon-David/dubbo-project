6d6a11fc76dc95de6df4f1d9f1506b5495630ee4
weiguo.zhang <xingyunruyubb@gmail.com>
2018-01-10 09:26:55 +0800

    页面

--- project-consumer/src/main/java/com/foryou/consumer/controller/user/UserController.java
+++ project-consumer/src/main/java/com/foryou/consumer/controller/user/UserController.java
@@ -1,10 +1,12 @@
 package com.foryou.consumer.controller.user;
 
 import com.foryou.consumer.process.user.UserProcess;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,10 +14,15 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "/user")
 public class UserController {
 
- private UserProcess userProcess;
-    @RequestMapping("/login")
+//    @Resource(name = "UserProcess")
+//    private UserProcess userProcess;
+
+
+    @RequestMapping(value = "/login")
     public void UserLogin(HttpServletRequest request, HttpServletResponse response, String callback,
-                          @RequestParam String json) throws Exception{
-        userProcess.login(request, response, callback, json);
+                          @RequestParam String json
+    ) throws Exception{
+        System.out.println("sdafadsf");
+//        userProcess.login(request, response, callback, json);
     }
 }
